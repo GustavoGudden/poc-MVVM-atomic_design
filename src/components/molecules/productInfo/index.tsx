320eae5770d7c9a3eb5351c873fd46162d88f657
Gustavo gudden <98347848+GustavoGudden@users.noreply.github.com>
2024-01-03 14:25:19 -0300

    Feat:ajustando mocks

--- src/components/molecules/productInfo/index.tsx
+++ src/components/molecules/productInfo/index.tsx
@@ -13,7 +13,7 @@ export const ProductInfo: FC<IProductInfo> = ({ onClickProductCard, data }) => {
 
   return (
     <div className="flex flex-col  justify-end px-4 py-2 border-t-2 " onClick={handleClick}>
-      <h1>{data.name}</h1>
+      <h1 className="font-medium text-[16px] text-black">{data.name}</h1>
       <div className="flex gap-1">
         <h2 className="font-medium line-through text-[14px] text-red-500">$120</h2>
         <h2 className="font-semibold ">{data.price}</h2>
