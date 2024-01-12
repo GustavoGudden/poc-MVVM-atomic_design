597011def720a2d0bfef9985dba933ff34d98b78
Gustavo gudden <98347848+GustavoGudden@users.noreply.github.com>
2024-01-12 14:01:54 -0300

    Fix: prouct responsive

--- src/components/molecules/productImageContainer/index.tsx
+++ src/components/molecules/productImageContainer/index.tsx
@@ -1,11 +1,10 @@
 import { FC } from 'react';
-import { ImageComponent } from '@/components/atoms/image';
 
 import Image from 'next/image';
 
 export const ProductImageContainer: FC = () => {
   return (
-    <div className=" relative xl:grow-[1]  xl:w-auto w-full h-[300px]  flex justify-center items-center  rounded-[4px] bg-gray-50">
+    <div className=" relative xl:grow-[1] xl:h-auto  xl:w-auto w-full h-[300px]  flex justify-center items-center  rounded-[4px] bg-gray-50">
       <Image src="/images/productImages/controllBanner.png" alt="" width={190} height={180} quality={100} />
     </div>
   );
