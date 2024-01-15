294eaf8bd18ef7d36e6bb31b2c71277b1d2c2947
Gustavo gudden <98347848+GustavoGudden@users.noreply.github.com>
2024-01-15 12:17:30 -0300

    Fix:responsive  sizes Text

--- src/components/molecules/fourBannerText/index.tsx
+++ src/components/molecules/fourBannerText/index.tsx
@@ -9,9 +9,9 @@ interface IFourBannerText {
 
 export const FourBannerText: FC<IFourBannerText> = ({ text, title }) => {
   return (
-    <div className="absolute xl:bottom-10 left-5 bottom-5 text-white">
-      <Text className="font-semibold text-[24px]">{title}</Text>
-      <Text className="font-normal text-[14px]">{text}</Text>
+    <div className="absolute xl:bottom-10 left-5 bottom-8 text-white">
+      <Text className="font-semibold xl:text-[24px] text-[16px]">{title}</Text>
+      <Text className="font-normal   xl:text-[14px] text-[12px]">{text}</Text>
       <Link href="/">
         <Text className="xl:text-[16px] font-medium underline underline-offset-[10px] text-[14px]">Shop Now</Text>
       </Link>
